77f5dce67ef8860c399b09aedcaf6f7010f45236
agcacruz <micahluis@yahoo.com.ph>
2019-01-09 14:58:26 +0800

    update css for chat page

--- client/src/components/chat/ChatIncoming.vue
+++ client/src/components/chat/ChatIncoming.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="incoming_msg">
     <div class="incoming_msg_img">
-      <img src="https://emoji.slack-edge.com/T91BPUL04/jodie_smiley_max/6f1c71bdf2523319.jpg">
+      <img src="https://emoji.slack-edge.com/T91BPUL04/jodie_smiley_max/6f1c71bdf2523319.jpg" width="28" height="28">
     </div>
     <div class="received_msg">
       <div class="received_withd_msg">
