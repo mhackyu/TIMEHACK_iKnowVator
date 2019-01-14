2755b101c1acfec3dd2512db82814242888a8dee
mhackyu <markpaderes0932@yahoo.com>
2019-01-14 17:26:06 +0800

    FIX: Move about page to home page

--- client/src/router.js
+++ client/src/router.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Auth from './views/Auth.vue';
 import Home from './views/Home.vue';
-import About from './views/About.vue';
 
 Vue.use(Router);
 
@@ -14,11 +13,6 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About,
     },
     {
       path: '/auth/:token',
