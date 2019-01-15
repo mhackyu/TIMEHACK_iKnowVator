a4c21c8350e5579b660d50e5851b0378e0bed9be
mhackyu <markpaderes0932@yahoo.com>
2019-01-15 10:44:56 +0800

    FEAT: Blacklisting token on logout FEAT: Checking of blacklisted token before executing db transactions

--- server/modules/users/index.js
+++ server/modules/users/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const user = require('./models/User');
+const bt = require('../auth/models/BlacklistedToken');
 const mw = require('../../middlewares/JWT');
 
 router.get('/', mw.verifyToken, (req, res) => {
@@ -15,6 +16,26 @@ router.get('/', mw.verifyToken, (req, res) => {
 
 router.get('/me', mw.verifyToken, (req, res) => {
   res.send(req.user);
+});
+
+router.post('/logout', mw.verifyToken, function(req, res) {
+  const data = {
+    token: req.token,
+    expires_at: req.tokExp
+  };
+  bt.create(data)
+    .then(result => {
+      if (result.affectedRows > 0) {
+        req.logout();
+        res.send({
+          status: 200,
+          msg: 'Successfully logged out.'
+        });
+      }
+    })
+    .catch(err => {
+      res.status(err.status).send(err);
+    });
 });
 
 module.exports = router;
