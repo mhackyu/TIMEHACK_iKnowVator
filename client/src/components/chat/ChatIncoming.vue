6beb7f1b7c5825a46690cd1a6c95de6f2e7b16d7
mhackyu <markpaderes0932@yahoo.com>
2019-01-16 20:21:07 +0800

    FIX: Move avatar to asset folder

--- client/src/components/chat/ChatIncoming.vue
+++ client/src/components/chat/ChatIncoming.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="incoming_msg">
     <div class="incoming_msg_img">
-      <img class="avatar" src="../img/avatar.png" style="height: 32px; width: 32px;">
+      <img class="avatar" src="../../assets/avatar.png" style="height: 32px; width: 32px;">
       <div class="received_msg">
         <div class="received_withd_msg " :class="{'has-list' : msg.list.length > 0}">
           <p>{{ msg.message }}</p>
