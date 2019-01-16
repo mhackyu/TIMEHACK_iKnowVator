5b395f186ad4a4109e5fa5794b9953f095a30cdf
agcacruz <micahluis@yahoo.com.ph>
2019-01-16 10:55:51 +0800

    add footer, add gif in about page

---
+++ client/src/components/footer/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="footer mt-auto py-3">
+    <div class="container">
+      <div class="col-md-12">
+        <span class="text-muted">iKnowVator</span>
+      </div>
+      <span class="text-muted">© 2010-2019</span>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+  .text-muted{
+    font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+  }
+  .footer{
+    background-color: #cdc9c9;
+  }
+</style>
